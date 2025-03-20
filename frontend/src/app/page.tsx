043e75fdab9d1b4c122b7b1e143ab8784e307c29
Otavio Mello <tatavoaugusto@hotmail.com>
2025-03-19 22:05:44 -0300

    Inicializando Projeto
    react + tailwind com next.js

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-gray-500 items-center justify-center">
+      <h1 className="font-bold underline" >Projeto Foda</h1>
+
+    </div>
+    
+  );
+}
