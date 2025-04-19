510c1a21972a6f27a7bf759801dc3f00fe84d6ec
Otavio Mello <tatavoaugusto@hotmail.com>
2025-04-19 15:20:51 -0300

    Definindo padrões
    definindo cores, fonts e variáveis

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-gray-500 items-center justify-center">
-      <h1 className="font-bold underline" >Projeto Foda</h1>
-
+    <div className="">
     </div>
     
   );
